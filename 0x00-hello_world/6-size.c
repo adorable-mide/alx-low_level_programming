55eaaee8df1f17346bea853c54e85eaa04cc4b0c
adorable-mide <oyekanmiayomide831@gmail.com>
2023-03-17 03:05:27 -0700

    /6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(a));
+/* sizeof evaluates the size of a variable */
+printf("Size of a char: %lu 1 byte(s)\n", (unsigned long)sizeof(ia));
 printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu 4 byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(d));
