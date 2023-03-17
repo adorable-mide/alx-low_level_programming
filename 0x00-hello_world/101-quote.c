e0457a3eedc31cc0f3b1019ebe1dcddaccb60d31
adorable-mide <oyekanmiayomide831@gmail.com>
2023-03-17 03:59:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return: 0 (Successful)
+ * main - function that prints out a specific line
+ *
+ * Return: 1 means success
  */
 int main(void)
 {
-puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",);
-return (0);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			stdout);
+	return (1);
 }
