9cb25cd5cc53bb7190f4af19f32b2722f6bd0b63
adorable-mide <oyekanmiayomide831@gmail.com>
2023-04-03 11:09:32 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int a = 0;
-
-    while (a < n) {
-        s[a] = b;
-        a++;
-    }
-    return s;
+	unsigned int a = 0;
+while (a < n)
+	{
+	s[a] = b;
+	a++;
+	}
+return (s);
 }
