cccb65ae2387879661816c30bebd56e3c8651c58
adorable-mide <oyekanmiayomide831@gmail.com>
2023-05-02 11:42:29 -0700

    mide

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+/**
+ * __p - prints no looped list.
+ *
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t __p(const listint_t *head)
+{
+	const listint_t *slow = head;
+	size_t num = 0;
+
+	while (slow != NULL)
+	{
+		num++;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+	}
+
+	return (num);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow = head;
+	const listint_t *fast = head;
+	size_t num = 0;
+	int count = 0;
+
+	if (head == NULL)
+		return (0);
+	if (head->next == NULL)
+		return (1);
+	do {
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			break;
+	} while (fast && fast->next);
+	if (slow != fast)
+		return (__p(head));
+
+	slow = head;
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	slow = head;
+	if (slow == fast)
+		count++;
+	while (count <= 1)
+	{
+		num++;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		if (slow == fast)
+			count++;
+	}
+	printf("-> [%p] %d\n", (void *)fast, fast->n);
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,95 @@
+#include "lists.h"
+
+/**
+ * __free - free a node
+ *
+ * @h: node
+ *
+ * Return: size of node
+ */
+size_t __free(listint_t **h)
+{
+	free(*h);
+	*h = NULL;
+	return (1);
+}
+
+/**
+ * __f - free normal linked list
+ *
+ * @h: node
+ *
+ * Return: size of freed list
+ */
+size_t __f(listint_t **h)
+{
+	if (*h == NULL)
+		return (0);
+
+	return (__f(&((*h)->next)) + __free(h));
+}
+
+/**
+ * __safe_free - free circular
+ * linked list
+ *
+ * @h: node
+ * @loop: node
+ * @count: count
+ *
+ * Return: size of freed list
+ */
+size_t __safe_free(listint_t **h, listint_t *loop, int count)
+{
+	if (*h == loop)
+	{
+		if (count == 2)
+			return (0);
+		count++;
+	}
+
+	return (__safe_free(&((*h)->next), loop, count) + __free(h));
+}
+
+/**
+ * free_listint_safe - free linked list
+ *
+ * @h: node
+ *
+ * Return: size of freed list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *slow = *h;
+	listint_t *fast = *h;
+	int count = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	if ((*h)->next == NULL)
+		return (__f(h));
+
+	do {
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			break;
+	} while (fast && fast->next);
+
+	if (slow != fast)
+		return (__f(h));
+
+	slow = *h;
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	if (slow == fast)
+		count++;
+
+	return (__safe_free(h, fast, count));
+}
